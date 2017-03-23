1708f9121c82f7a511dbb77cf4f776131830a2ae
SangeethaBabu <sangeetha.babu@rapidvaluesolutions.com>
2017-03-23 12:08:12 +0530

    Edited

---
+++ CC-test-master/new-test.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+		<h1>New Test</h1>
+		<div>
+			<?php 
+				echo "Happy New Year";
+			?>
+			<p>edited new version</p>
+		</div>
+	</body>
+</html>
---
+++ CC-test-master/one.php
@@ -0,0 +1,17 @@
+<html>
+	<body>
+		<p>
+			<?php
+				echo "one";
+			?>
+			
+		</p>
+		<ul>
+			<li>One</li>
+			<li>Two</li>
+			<li>Three</li>
+			<li>Four</li>
+			<li>Five</li>
+		</ul>
+	</body>
+</html>
